90ea5911722df6fd55bdd290c114411d36df1396
TowhidMolla-Dev <towhidm7.4@gmail.com>
2023-10-12 23:18:48 +0600

    Initial

---
+++ app/src/main/java/com/towhid/myjournal/Adapters/NotesListAdapter.java
@@ -0,0 +1,9 @@
+package com.towhid.myjournal.Adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotesListAdapter {
+}
+class NotesViewHolder extends RecyclerView.ViewHolder {
+
+}
