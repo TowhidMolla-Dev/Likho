de762347d4999f9d4dc955a64fba73697f3ceb34
TowhidMolla-Dev <towhidm7.4@gmail.com>
2023-10-12 16:00:42 +0600

    dd name, getinstance

--- app/src/main/java/com/towhid/myjournal/Database/RoomDB.java
+++ app/src/main/java/com/towhid/myjournal/Database/RoomDB.java
@@ -3,15 +3,25 @@ package com.towhid.myjournal.Database;
 import android.content.Context;
 
 import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import com.towhid.myjournal.Models.Notes;
 
 @Database(entities = Notes.class, version = 1, exportSchema = false)
-public abstract class RoomDB {
+public abstract class RoomDB extends RoomDatabase {
     private static RoomDB database;
     private static String DATABASE_NAME = "NoteAPP";
 
-    public synchronized static RoomDB getInstance(Context context){
-
+    public synchronized static RoomDB getInstance(Context context) {
+        if (database == null){
+            database = Room.databaseBuilder(context.getApplicationContext(),
+                    RoomDB.class, DATABASE_NAME)
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return database;
     }
+    public abstract MainDAO mainDAO();
 }
