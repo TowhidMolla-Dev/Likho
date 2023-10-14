43fbfdcf2f13ebc81b640a35d9b737d83bab4596
TowhidMolla-Dev <towhidm7.4@gmail.com>
2023-10-14 11:57:41 +0600

    update @Insert

--- app/src/main/java/com/towhid/myjournal/Database/MainDAO.java
+++ app/src/main/java/com/towhid/myjournal/Database/MainDAO.java
@@ -13,15 +13,15 @@ import java.util.List;
 
 @Dao
 public interface MainDAO {
-    @Insert(onConflict = REPLACE)
+    @Insert
     void insert(Notes notes);
 
     @Query("SELECT * From notes ORDER BY ID DESC")
     List<Notes> getAll();
 
-    @Query("UPDATE notes SET title = :title, notes = :notes WHERE ID = :id");
-    void update(int id, String title, String notes);
+    @Query("UPDATE notes SET title = :title, notes = :notes WHERE ID = :id")
+    public void update(int id, String title, String notes);
 
     @Delete
-    void delete(Notes notes);
+    public void delete(Notes notes);
 }
